2a010a8ec0970dcded449c791dea98711acdb544
mitsune <tho.giotti@gmail.com>
2018-10-29 14:09:35 +0100

    utilisation des fixtures

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
 
 class BlogController extends AbstractController
 {
@@ -12,8 +13,11 @@ class BlogController extends AbstractController
      */
     public function index()
     {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $repo->findAll();
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
     }
 
@@ -26,11 +30,14 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route ("/blog/12", name="blog_show")
+     * @Route ("/blog/{id}", name="blog_show")
      */
-    public function show()
+    public function show($id)
     {
-        return $this->render('blog/show.html.twig');
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $article = $repo->find($id);
+        return $this->render('blog/show.html.twig', [
+            'article' => $article]);
     }
 
 }
--- src/DataFixtures/ArticlesFixtures.php
+++ src/DataFixtures/ArticlesFixtures.php
@@ -4,15 +4,20 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use app\Entity\Article;
 
 class ArticlesFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
         for ($i = 1; $i<=10; $i ++){
-
+            $article = new Article();
+            $article->setTitle("Titre de l'article n:$i");
+            $article->setContent("<p>contenu de l'article n:$i</p>");
+            $article->setImage("http://placehomd.it/350x150");
+            $article->setCreatedAt(new \DateTime());
+            $manager->persist($article);
         }
-
         $manager->flush();
     }
 }
