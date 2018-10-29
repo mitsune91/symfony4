08be723fff6cf0ca01cd466fec40934a08e2d86b
mitsune <tho.giotti@gmail.com>
2018-10-29 17:56:23 +0100

    Mise a jour du controller avec le formulaire de creation d'article + creation de la vue pour ce formulaire

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,9 +3,18 @@
 namespace App\Controller;
 
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use function Sodium\add;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+
 
 
 class BlogController extends AbstractController
@@ -32,6 +41,22 @@ class BlogController extends AbstractController
     }
 
     /**
+     * @Route("/blog/new", name="blog_create")
+     */
+    public function create(Request $request,ObjectManager $manager)
+    {
+        $article = new Article();
+        $form = $this->createFormBuilder($article)
+            ->add('title')
+            ->add('content')
+            ->add('image')
+            ->getForm();
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView()
+        ]);
+    }
+
+    /**
      * @Route ("/blog/{id}", name="blog_show")
      */
     public function show(Article $article)
@@ -41,5 +66,6 @@ class BlogController extends AbstractController
         return $this->render('blog/show.html.twig', [
             'article' => $article]);
     }
+
 
 }
